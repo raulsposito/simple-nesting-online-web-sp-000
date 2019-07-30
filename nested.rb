7e866e710ea23086e9eedfb616d4f5f5b7191dee
Raul Sposito <raulsposito@gmail.com>
2019-07-30 13:46:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -96,9 +96,9 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     programmer_hash[:alan_kay][:known_for] = alans_new_info
-     alans_new_info = "GUI"
      
+     alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      programmer_hash
 end
 
