3ea2a0906c55f3c93deef8b43fd1c014cb0f89a7
Raul Sposito <raulsposito@gmail.com>
2019-07-30 13:46:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -96,10 +96,10 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      alans_new_info = "GUI"
      
-     
+     programmer_hash
 end
 
 def adding_to_dennis
